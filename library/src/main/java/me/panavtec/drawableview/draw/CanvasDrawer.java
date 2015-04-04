3481f24857f1edfb72e1fe2607d757a701a11101
Christian Panadero Martinez <panavtec@gmail.com>
2015-04-04 19:05:31 +0200

    Added canvas logger

--- library/src/main/java/me/panavtec/drawableview/draw/CanvasDrawer.java
+++ library/src/main/java/me/panavtec/drawableview/draw/CanvasDrawer.java
@@ -3,6 +3,7 @@ package me.panavtec.drawableview.draw;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import me.panavtec.drawableview.BuildConfig;
 
 public class CanvasDrawer {
 
@@ -14,8 +15,9 @@ public class CanvasDrawer {
   public CanvasDrawer() {
     initPaint();
   }
-  
+
   public void onDraw(Canvas canvas) {
+    log(canvas);
     canvas.drawRect(canvasRect, paint);
     canvas.scale(scaleFactor, scaleFactor);
     canvas.translate(-viewRect.left, -viewRect.top);
@@ -24,7 +26,6 @@ public class CanvasDrawer {
   public void onScaleChange(float scaleFactor) {
     this.scaleFactor = scaleFactor;
   }
-
   public void onViewPortChange(RectF viewRect) {
     this.viewRect = viewRect;
   }
@@ -37,5 +38,23 @@ public class CanvasDrawer {
     paint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.DITHER_FLAG | Paint.FILTER_BITMAP_FLAG);
     paint.setStrokeWidth(2.0f);
     paint.setStyle(Paint.Style.STROKE);
+    paint.setTextSize(25.0f);
+  }
+
+  private void log(Canvas canvas) {
+    if (BuildConfig.DEBUG) {
+      RectF notScaled = new RectF(canvasRect);
+      notScaled.right /= scaleFactor;
+      notScaled.bottom /= scaleFactor;
+      int lineNumber = 0;
+      logLine(canvas, "Canvas: " + canvasRect.toShortString(), ++lineNumber);
+      logLine(canvas, "No scaled canvas: " + notScaled.toShortString(), ++lineNumber);
+      logLine(canvas, "View: " + viewRect.toShortString(), ++lineNumber);
+      logLine(canvas, "Scale factor: " + scaleFactor + "x", ++lineNumber);
+    }
+  }
+
+  private void logLine(Canvas canvas, String text, int lineNumber) {
+    canvas.drawText(text, 5, 30 * lineNumber, paint);
   }
 }
